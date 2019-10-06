c5031d6a7a0d1d46581336269b97debb7b6f5310
Prashanth Naika <pnaika@egen.solutions>
2019-10-06 14:28:19 -0700

    server update

--- server.js
+++ server.js
@@ -1,81 +1,11 @@
 const express = require('express');
 const app = express();
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-const _ = require('lodash');
+const path = require('path');
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(express.static(__dirname + '/dist/'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist//index.html'));
 });
 
-app.get('/', async (req, res) => {
-    let crimeData = [];
-    try {
-        crimeData = await fetch(`https://data.cityofchicago.org/resource/crimes.json`)
-            .then((res) => {
-                return res.json();
-            });
-    } catch (e) {
-        return e;
-    }
-    res.send(crimeData)
-});
-
-app.get('/details/:id', async (req, res) => {
-    const id = req.params.id || '';
-    let crimeData = [];
-    try {
-        crimeData = await fetch(`https://data.cityofchicago.org/resource/crimes.json?case_number=${id}`)
-            .then((res) => {
-                return res.json();
-            });
-    } catch (e) {
-        return e;
-    }
-
-    res.send(crimeData)
-});
-
-app.get('/primaryTypes', async (req, res) => {
-    let crimeData = [];
-    let primaryType = [];
-
-    try {
-        crimeData = await fetch(`https://data.cityofchicago.org/resource/crimes.json`)
-            .then((res) => {
-                return res.json();
-            });
-
-        primaryType = _.uniq(_.map(crimeData, 'primary_type'));
-    } catch (e) {
-        return e;
-    }
-
-    res.send(primaryType)
-});
-
-
-app.get('/primaryTypes/:primaryType/:years?', async (req, res) => {
-    const primaryType = req.params.primaryType || '';
-    const years = req.params.years || '2018';
-    let crimeData = [];
-    try {
-        crimeData = await fetch(`https://data.cityofchicago.org/resource/crimes.json?primary_type=${primaryType}&year=${years}`)
-            .then((res) => {
-                return res.json();
-            });
-    } catch (e) {
-        return e;
-    }
-
-    res.send(crimeData)
-});
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Listening to ${port}`)
-});
+console.log('Console Listing !!');
